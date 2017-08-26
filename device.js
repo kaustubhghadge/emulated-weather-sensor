618945643a8f95f744362464ad00aed78029c75c
Kaustubh Ghadge <kaustubhghadge@outlook.com>
2017-08-26 12:44:57 -0500

    setting min and max values for humidity and temperature

--- device.js
+++ device.js
@@ -7,6 +7,13 @@ Emulating a IoT Device sending it's ID, Temperature in Farhenheit and Humidity i
 var env = process.env.NODE_ENV || 'development',
     config = require('./env')[env];
 
+
+var max_temperature = config.max_temperature,
+	min_temperature=config.min_temperature,
+	max_humidity=config.max_humidity,
+	min_humidity=config.min_humidity;
+
+
 //get random values for temperature and humidity
 function getRandomValues(min,max){
 
@@ -34,8 +41,8 @@ function getRandomValues(min,max){
 function weatherInfo(){
 
 	var	 deviceId="1234ABCD",
-		 temperature = getRandomValues(60,80),
-		 humidity = Math.round(getRandomValues(30,60));
+		 temperature = getRandomValues(min_temperature,max_temperature),
+		 humidity = Math.round(getRandomValues(min_humidity,max_humidity));
 
 
 	var weather = {
@@ -58,7 +65,6 @@ const wss = new WebSocket.Server({ port: config.port });
 
 //opening the ws connection and sending the data
 wss.on('connection', function connection(ws) {
-
   ws.send(weatherInfo());
   setInterval(function(){ws.send(weatherInfo())},config.interval)
 });
--- env.js
+++ env.js
@@ -8,6 +8,10 @@
 module.exports = {
 	development:{
 	port: 9090,
-	interval:6000
+	interval:6000,
+	max_temperature:80,
+	min_temperature:60,
+	max_humidity:60,
+	min_humidity:30
 }
 }
