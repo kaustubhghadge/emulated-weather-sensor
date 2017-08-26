f8aaa75de82faedfd199b00fa5a8b614b0e03d87
Kaustubh Ghadge <kaustubhghadge@outlook.com>
2017-08-26 10:59:35 -0500

    using websockets, removed unnecessary restify

--- device.js
+++ device.js
@@ -4,7 +4,8 @@ Emulating a IoT Device sending it's ID, Temperature in Farhenheit and Humidity i
 
 */
 
-
+var env = process.env.NODE_ENV || 'development',
+    config = require('./env')[env];
 
 //get random values for temperature and humidity
 function getRandomValues(min,max){
@@ -46,7 +47,16 @@ function weatherInfo(){
 	return (weather);
 }
 
+//initiating websockets
+const WebSocket = require('ws');
 
-module.exports= {
-	weatherInfo:weatherInfo
-}
+//starting a websockets server at defined port
+const wss = new WebSocket.Server({ port: config.port });
+
+//getting the weather data as an object
+weatherData=weatherInfo();
+
+//opening the ws connection and sending the data in string format
+wss.on('connection', function connection(ws) {
+  ws.send(JSON.stringify(weatherData));
+});
